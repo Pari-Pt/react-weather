ddaa62d9b3e86351e3a1c3072ebf116e50deb48f
Seo-Pt <seonah.pari.pt@gmail.com>
2022-05-09 15:20:00 +0100

    Created and imported new WeatherForecast component

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -108,6 +108,7 @@ export default function WeatherSearch(props) {
         </div>
             <DateTime timestamp={weatherData.date}/>
             <WeatherData data={weatherData} />
+            <WeatherForecast />
   </div>
    );} 
   
