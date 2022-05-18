e5fda55dbc2c365faae0b6b96ad55b1941af692e
Seo-Pt <seonah.pari.pt@gmail.com>
2022-05-18 11:48:16 +0100

    Added States to forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,24 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 import WeatherIcon from "./WeatherIcon.js";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props){
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
 
     function handleResponse(response){
-            
+        setLoaded(true);
+        setForecast(response.data.daily);
     }
     
-    let latitude = props.coordinates.lat;
-    let longitude = props.coordinates.lon;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly,alerts&appid=8a986f69d218ec337be3c1ef2d26c6e8&units=metric`;
-    
-    axios.get(apiUrl).then(handleResponse);
-    
-    
-    return (
-    <div className="WeatherForecast">
+    if (loaded) {
+console.log(forecast[1].temp.max);
+        return (
+            <div className="WeatherForecast">
         <div className="row">
             <div className="col">
                 <div className="day-name">Mon</div>
@@ -30,5 +28,16 @@ export default function WeatherForecast(props){
             </div>
         </div>
     </div>
-)    
+        );
+
+    } else {
+        let latitude = props.coordinates.lat;
+        let longitude = props.coordinates.lon;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly,alerts&appid=8a986f69d218ec337be3c1ef2d26c6e8&units=metric`;
+        
+        axios.get(apiUrl).then(handleResponse);
+        
+        return (null);
+
+    }
 }
