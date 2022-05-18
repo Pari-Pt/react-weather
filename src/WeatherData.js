ff267d9c4f2f6cc28e4aae62534a8f7310ada8a5
Seo-Pt <seonah.pari.pt@gmail.com>
2022-05-18 10:44:55 +0100

    Changed layout of displayed weather data

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -4,6 +4,7 @@ import Temperature from "./Temperature.js";
 import "./WeatherData.css";
 
 export default function WeatherData(props){
+
 return(
      <div className="WeatherData">
         
@@ -16,17 +17,19 @@ return(
         <div className="hstack gap-3 align-middle">
             <Temperature temp={props.data.temperature}/>
             <div className="vr"></div>
+            <WeatherIcon icon={props.data.iconCode} size={65}/>
+            <div className="vr"></div>
             <div className="vstack">
             <div className="sub-weather" id="humidity">Humidity: {props.data.humidity}%</div>
             <div className="sub-weather" id="wind-speed">Speed: {props.data.wind}m/s</div> 
             </div>
+            
             </div>
-            <WeatherIcon icon={props.data.iconCode} size={65}/>
+            </div>
             
             
 
-        </div>
-        
+  
 
     );
 }
